066641b54cf91620ea49ccd7208b5ba61ef9de83
KyawThetKhing <ktkhaing47@gmail.com>
2025-03-07 14:41:23 +0700

    feat: add validation with zod

--- components/transaction-model.vue
+++ components/transaction-model.vue
@@ -1,44 +1,129 @@
 <script setup>
+import { z } from 'zod';
 import { categories, types } from '@/constants';
-const model = defineModel();
+const isOpen = defineModel();
+
+const state = ref({
+  type: undefined,
+  amount: 0,
+  created_at: undefined,
+  description: undefined,
+  category: undefined,
+});
+
+// const state = ref({
+//   ...initialState,
+// });
+
+const resetForm = () => {
+  state.value = {
+    type: undefined,
+    amount: 0,
+    created_at: undefined,
+    description: undefined,
+    category: undefined,
+  };
+};
+
+const form = ref();
+
+const defaultSchema = z.object({
+  created_at: z.string(),
+  description: z.string().optional(),
+  amount: z.number().positive('Amount must be greater than 0'),
+});
+const incomeSchema = z.object({
+  type: z.literal('Income'),
+});
+const expenseSchema = z.object({
+  type: z.literal('Expense'),
+  category: z.enum(categories),
+});
+const investmentSchema = z.object({
+  type: z.literal('Investment'),
+});
+const savingSchema = z.object({
+  type: z.literal('Saving'),
+});
+const schema = z.intersection(
+  z.discriminatedUnion('type', [
+    incomeSchema,
+    expenseSchema,
+    investmentSchema,
+    savingSchema,
+  ]),
+  defaultSchema
+);
+
+const save = async () => {
+  if (form.value.errors.length > 0) return;
+};
+
+watch(isOpen, (newVal) => {
+  if (newVal) resetForm();
+});
 </script>
 <template>
-  <UModal v-model="model">
+  <UModal v-model="isOpen">
     <UCard>
       <template #header> Add Transaction </template>
-      <UFormGroup
-        required="true"
-        label="Transaction Type"
-        name="type"
-        class="mb-4"
-      >
-        <USelect placeholder="Transaction Type" :options="types" />
-      </UFormGroup>
+      <UForm :state="state" :schema="schema" ref="form" @submit.prevent="save">
+        <UFormGroup
+          required="true"
+          label="Transaction Type"
+          name="type"
+          class="mb-4"
+        >
+          <USelect
+            placeholder="Transaction Type"
+            :options="types"
+            v-model="state.type"
+          />
+        </UFormGroup>
 
-      <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
-        <UInput type="number" placeholder="Amount" />
-      </UFormGroup>
-      <UFormGroup
-        label="Transaction date"
-        :required="true"
-        name="created_at"
-        class="mb-4"
-      >
-        <UInput type="date" icon="i-heroicons-calendar-days-20-solid" />
-      </UFormGroup>
-      <UFormGroup
-        label="Description"
-        hint="Optional"
-        name="description"
-        class="mb-4"
-      >
-        <UTextarea placeholder="Description" />
-      </UFormGroup>
-      <UFormGroup required="true" label="Category" name="category" class="mb-4">
-        <USelect placeholder="Category" :options="categories" />
-      </UFormGroup>
+        <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
+          <UInput
+            type="number"
+            placeholder="Amount"
+            v-model.number="state.amount"
+          />
+        </UFormGroup>
+        <UFormGroup
+          label="Transaction date"
+          :required="true"
+          name="created_at"
+          class="mb-4"
+        >
+          <UInput
+            type="date"
+            icon="i-heroicons-calendar-days-20-solid"
+            v-model="state.created_at"
+          />
+        </UFormGroup>
+        <UFormGroup
+          label="Description"
+          hint="Optional"
+          name="description"
+          class="mb-4"
+        >
+          <UTextarea placeholder="Description" v-model="state.description" />
+        </UFormGroup>
+        <UFormGroup
+          required="true"
+          label="Category"
+          name="category"
+          class="mb-4"
+          v-if="state.type === 'Expense'"
+        >
+          <USelect
+            placeholder="Category"
+            :options="categories"
+            v-model="state.category"
+          />
+        </UFormGroup>
 
-      <UButton type="submit" color="black" variant="solid" label="Save" />
+        <UButton type="submit" color="black" variant="solid" label="Save" />
+      </UForm>
     </UCard>
   </UModal>
 </template>
