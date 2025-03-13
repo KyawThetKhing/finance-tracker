cbdaca21594dec5d26cc37330b74b9133619ad09
KyawThetKhing <ktkhaing47@gmail.com>
2025-03-13 14:03:49 +0700

    feat: save user profile to supabase

--- pages/settings/profile.vue
+++ pages/settings/profile.vue
@@ -8,7 +8,7 @@ const { toastSuccess, toastError } = useAppToast();
 const pending = ref(false);
 
 const state = ref({
-  name: '',
+  name: user.value.user_metadata?.full_name || '',
   email: user.value.email,
 });
 
@@ -16,9 +16,37 @@ const schema = z.object({
   name: z.string().min(2).optional(),
   email: z.string().email(),
 });
+
+const saveProfile = async () => {
+  pending.value = true;
+  try {
+    const data = {
+      data: {
+        full_name: state.value.name,
+      },
+    };
+    if (state.value.email !== user.value.email) {
+      data.email = state.value.email;
+    }
+
+    const { error } = await supabase.auth.updateUser(data);
+    if (error) throw error;
+    toastSuccess({
+      title: 'Profile updated successfully',
+      description: 'Your profile has been updated!!',
+    });
+  } catch (error) {
+    toastError({
+      title: 'Error updating profile',
+      description: error.message,
+    });
+  } finally {
+    pending.value = false;
+  }
+};
 </script>
 <template>
-  <UForm :state="state" :schema="schema">
+  <UForm :state="state" :schema="schema" @submit.prevent="saveProfile">
     <UFormGroup class="mb-4" label="Full Name" name="name">
       <UInput v-model="state.name" />
     </UFormGroup>
@@ -37,7 +65,8 @@ const schema = z.object({
       color="black"
       variant="solid"
       label="Save"
-      :pending="pending"
+      :loading="pending"
+      :disabled="pending"
     />
   </UForm>
 </template>
