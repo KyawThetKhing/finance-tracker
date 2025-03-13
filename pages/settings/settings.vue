6e65b96e26874efd66e091ae7771aeb0680087bb
KyawThetKhing <ktkhaing47@gmail.com>
2025-03-13 14:43:51 +0700

    feat: save user setting to supabase

--- pages/settings/settings.vue
+++ pages/settings/settings.vue
@@ -1,3 +1,63 @@
+<script setup>
+import { z } from 'zod';
+import { transactionViewOptions } from '@/constants';
+
+const supabase = useSupabaseClient();
+const user = useSupabaseUser();
+
+const state = ref({
+  seletedView:
+    user.value.user_metadata?.transaction_view || transactionViewOptions[1],
+});
+const pending = ref(false);
+const { toastSuccess, toastError } = useAppToast();
+
+const schema = z.object({
+  seletedView: z.enum(transactionViewOptions),
+});
+
+const saveSettings = async () => {
+  pending.value = true;
+  console.log('State', state.value);
+  try {
+    const { error } = await supabase.auth.updateUser({
+      data: {
+        transaction_view: state.value.seletedView,
+      },
+    });
+    if (error) throw error;
+    toastSuccess({
+      title: 'Settings saved successfully',
+      description: 'Your settings have been saved!!',
+    });
+  } catch (error) {
+    toastError({
+      title: 'Error saving settings',
+      description: error.message,
+    });
+  } finally {
+    pending.value = false;
+  }
+};
+</script>
 <template>
-  <div>Settings</div>
+  <UForm :state="state" :schema="schema" @submit.prevent="saveSettings">
+    <UFormGroup
+      label="Transaction View"
+      class="mb-4"
+      help="Choose how you want to show your transactions"
+    >
+      <USelect v-model="state.seletedView" :options="transactionViewOptions" />
+    </UFormGroup>
+
+    <UButton
+      type="submit"
+      color="white"
+      variant="solid"
+      :loading="pending"
+      :disabled="pending"
+    >
+      Save
+    </UButton>
+  </UForm>
 </template>
